921db9b070af5bbfa1e578972717657f1cbf6b89
Andrej <an.sharapov90@gmail.com>
2018-02-26 16:48:09 +0300

    footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
-<footer class="py-5 bg-dark text-white">
+<footer class="footer py-5 bg-dark text-white">
 	<div class="container">	
 		
       <div class="row">
         <div class="col-12 col-md text-center">
-          <img src="white_logo_on_transparent_156x73.png" alt="">
+          <img src="/white_logo_on_transparent_156x73.png" alt="">
           <small class="d-block mb-3 text-muted">&copy; <?php echo date('Y'); ?> <a class="text-muted" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></small>
         </div>
           <?php
