756527cad3b185c6ca3fee050b88cd08c73098bc
Andrej <an.sharapov90@gmail.com>
2018-02-26 16:47:16 +0300

    Create page.php

---
+++ page.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<div class="container bg-white my-3">
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb bg-white">
+    <li class="breadcrumb-item"><a href="/">Главная</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Поиск</li>
+  </ol>
+</nav>
+</div>
+
+<div class="container bg-white my-3 py-3">
+    <h1><?php the_title(); ?></h1>
+    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+    <?php the_content(); ?>
+    <?php endwhile; endif; ?>
+</div>
+<?php get_footer(); ?>
