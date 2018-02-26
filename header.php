33d6b3b94290dadd6721037ab939681738310718
Andrej <an.sharapov90@gmail.com>
2018-02-26 11:25:15 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,36 +1,48 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="description" content="<?php if ( is_single() ) {
+      single_post_title('', true);
+    } else {
+      bloginfo('name'); echo " - "; bloginfo('description');
+    }
+  ?>" />
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta name="description" content="demo website creative agency madeas studio" />
     <title><?php wp_title('«', true, 'right'); ?> <?php bloginfo('name'); ?></title>	
 	<link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	<?php wp_head(); ?>      
+  <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>	
-		
-<header>	
-<nav class="navbar navbar-topbar sticky-top navbar-dark bg-dark">
-    <a class="navbar-brand" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a>
-  <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
-    &#9776;
-  </button>
-  <div class="collapse navbar-toggleable-xs" id="collapsingNavbar">
-    <?php
+
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdown" aria-controls="navbarDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="<?php echo esc_url( home_url('/') ); ?>"><?php bloginfo('name'); ?></a>
+
+    <div class="collapse navbar-collapse" id="navbarDropdown">
+      <?php
         wp_nav_menu( array(
-          'theme_location' => 'navbar',
-          'container'      => false,
-          'menu_class'     => 'nav navbar-nav',
-          'fallback_cb'    => '__return_false',
-          'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-          'depth'          => 2,
-          'walker'         => new bootstrap_4_walker_nav_menu()
-       ) );
-    ?>
-    <?php get_template_part('navbar-search'); ?>
-  </div>
+          'theme_location'  => 'navbar',
+          'container'       => false,
+          'menu_class'      => '',
+          'fallback_cb'     => '__return_false',
+          'items_wrap'      => '<ul id="%1$s" class="navbar-nav mr-auto mt-2 mt-lg-0 %2$s">%3$s</ul>',
+          'depth'           => 2,
+          'walker'          => new bootstrap_4_walker_nav_menu()
+        ) );
+      ?>
+
+      <form class="form-inline ml-auto pt-2 pt-md-0" role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+        <input class="form-control mr-sm-1" type="text" value="<?php echo get_search_query(); ?>" placeholder="Search..." name="s" id="s">
+        <button type="submit" id="searchsubmit" value="<?php esc_attr_x('Search', 'b4st') ?>" class="btn btn-outline-secondary my-2 my-sm-0">
+          <i class="fas fa-search"></i>
+        </button>
+      </form>
+    </div>
 </nav>
-</header>
