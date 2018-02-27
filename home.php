404fd7995132c3e03b57e9402dcc7b06ed2dcec6
Andrej <an.sharapov90@gmail.com>
2018-02-27 15:00:08 +0300

    home.php

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <div class="container bg-white my-3">
 <nav aria-label="breadcrumb">
   <ol class="breadcrumb bg-white">
-    <li class="breadcrumb-item"><a href="/">Главная</a></li>
+    <li class="breadcrumb-item"><a href="/"><?php bloginfo('name'); ?></a></li>
     <li class="breadcrumb-item active" aria-current="page"><?php the_title(); ?></li>
   </ol>
 </nav>
@@ -33,25 +33,7 @@
 
       <?php endwhile; ?>
 
-      <nav>
-        <ul class="pagination">
-          <li>
-            <a href="#" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-            </a>
-          </li>
-          <li><a href="#">1</a></li>
-          <li><a href="#">2</a></li>
-          <li><a href="#">3</a></li>
-          <li><a href="#">4</a></li>
-          <li><a href="#">5</a></li>
-          <li>
-            <a href="#" aria-label="Next">
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
-      </nav>			
+      <?php if (function_exists('theme_pagination')) theme_pagination(); ?>	
         </div>
 		
         <?php get_sidebar(); ?>
