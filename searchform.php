09cd586f98dcce1a3d2548fbe8d2e1c5ad0031de
Andrej <an.sharapov90@gmail.com>
2018-02-27 12:05:51 +0300

    searchform.php

--- searchform.php
+++ searchform.php
@@ -12,5 +12,5 @@
 <?php $unique_id = esc_attr( uniqid( 'searchform' ) ); ?>
 <form role="search" method="get" id="searchform" class="form-inline my-2 my-lg-0 searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <input type="text" id="<?php echo $unique_id; ?>" class="form-control mr-sm-2" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'tWPonB4' ); ?>" name="s">
-    <button class="btn btn-outline-secondary my-2 my-sm-0" id="searchsubmit" type="submit"><?php echo twentyseventeen_get_svg( array( 'icon' => 'search' ) ); ?>Поиск</button>
+    <button class="btn btn-outline-secondary my-2 my-sm-0" id="searchsubmit" type="submit">Поиск</button>
 </form>
