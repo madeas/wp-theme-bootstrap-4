12ddc5649df89ce5021a6226ae76a2871c77a2d5
Andrej <an.sharapov90@gmail.com>
2018-02-27 14:21:54 +0300

    header.php

--- header.php
+++ header.php
@@ -12,25 +12,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta name="description" content="demo website creative agency madeas studio" />
-    <title><?php wp_title('«', true, 'right'); ?> <?php bloginfo('name'); ?></title>	
+    <title><?php wp_title('-', true, 'right'); ?> <?php bloginfo('name'); ?></title>	
 	<link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>	
-<header itemscope itemtype="http://schema.org/WPHeader" class="header">
-	
-<div class="bg-white header-widgets">
-    <div class="container">
-		<div class="row very-top-header">
-          <?php
-if ( is_active_sidebar( 'custom-header-widget' ) ) : ?>
-	<?php dynamic_sidebar( 'custom-header-widget' ); ?>
-<?php endif; ?>
-		</div>
-    </div>
-</div>
-	
+<header itemscope itemtype="http://schema.org/WPHeader" class="header">	
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdown" aria-controls="navbarDropdown" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"></span>
