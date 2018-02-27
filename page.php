13c71d9356a5edbb78cc04cf173fa8fcaaa18353
Andrej <an.sharapov90@gmail.com>
2018-02-27 12:46:50 +0300

    Update page.php

--- page.php
+++ page.php
@@ -1,4 +1,14 @@
-<?php get_header(); ?>
+<?php 
+/**
+ * Шаблон для отображения всех страниц
+
+ * @package WordPress
+ * @subpackage tWPonB4
+ * @since 1.0
+ * @version 1.0
+ */
+get_header(); ?>
+
 <div class="container bg-white my-3">
 <nav aria-label="breadcrumb">
   <ol class="breadcrumb bg-white">
@@ -8,9 +18,16 @@
 </nav>
 </div>
 <div class="container bg-white my-3 py-3">
-    <h1><?php the_title(); ?></h1>
-    <?php if (have_posts()): while (have_posts()): the_post(); ?>
-    <?php the_content(); ?>
-    <?php endwhile; endif; ?>
+
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				get_template_part( 'template-parts/page/content', 'page' );
+				// Если комментарии открыты или у нас есть хотя бы один комментарий, загрузите шаблон комментария.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			endwhile; // End of the loop.
+			?>
 </div>
 <?php get_footer(); ?>
