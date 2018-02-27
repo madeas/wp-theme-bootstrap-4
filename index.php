ec2cd079a66e0cc43d5f05b65578c1ef00e883d8
Andrej <an.sharapov90@gmail.com>
2018-02-27 09:13:29 +0300

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 
+<?php get_header(); ?>
+<div class="container bg-white my-3">
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb bg-white">
+    <li class="breadcrumb-item"><a href="/">Главная</a></li>
+    <li class="breadcrumb-item active" aria-current="page"><?php the_title(); ?></li>
+  </ol>
+</nav>
+</div>
+<div class="container bg-white my-3 py-3">
+        <div class="row">
+            <div class="col-md-8">
+            <?php get_template_part( 'loop', 'single' );?>
+            </div>
+			<?php if ( is_active_sidebar( 'sidebar' ) ) : ?> 
+			<div id="sidebar" class="sidebar"> 
+			<?php dynamic_sidebar( 'sidebar' ); ?> 
+			</div> 
+			<?php endif; ?>
+        </div>
+    </div>
+<?php get_footer(); ?>
