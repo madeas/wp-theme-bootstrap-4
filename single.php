fdfe57579fdacc34857d58a2e770194cf2705f28
Andrej <an.sharapov90@gmail.com>
2018-02-28 14:56:27 +0300

    single.php
    Страница записи

--- single.php
+++ single.php
@@ -1,28 +1,25 @@
 <?php get_header(); ?>
-    <div id="container">
-        <div class="row">
-            <div class="col-md-8">            
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-				get_template_part( 'template-parts/post/content', get_post_format() );
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-				the_post_navigation(
-					array(
-						'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
-						'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
-					)
-				);
-			endwhile; // End of the loop.
+<div class="container bg-white my-3 py-3">
+	<div class="row">
+		
+		<div class="col-md-12">			
+		<h2>
+		<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+		</h2>
+			<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part('content', 'single') ?>
+			
+			<?php // If comments are open or we have at least one comment, load up the comment template.
+ 				if ( comments_open() || '0' != get_comments_number() ) :
+ 					comments_template();
+ 				endif;
 			?>
+			
+			<?php endwhile; ?>
+		</div>
+		
+	</div>
+</div>
 
-            </div>
-                <?php get_sidebar(); ?>
-        </div>
-    </div>
 <?php get_footer(); ?>
