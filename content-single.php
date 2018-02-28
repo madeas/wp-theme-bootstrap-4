47003c8593b25b5f664d3b01ce587966c2b48197
Andrej <an.sharapov90@gmail.com>
2018-02-28 14:46:48 +0300

    content-single.php
    Содержание страницы записи

--- content-single.php
+++ content-single.php
@@ -1,5 +1,9 @@
+<article class="is-post is-post-excerpt" class="<?php post_class(); ?>" id="post-<?php the_ID(); ?>">
+       <!-- <?php if ( has_post_thumbnail() ) {the_post_thumbnail();}  ?> -->
+<?php the_content(); ?>
 <footer class="entry-meta">
 	<?php edit_post_link( __( 'Edit', 'striped' ), '<div class="edit-link">', '</div>' ); ?>
 	<p class="tags"><?php the_tags(__( 'Tagged as: ', 'striped' ),', '); ?></p>
 	<p class="categories"><?php _e( 'Categorized in&#58; ', 'striped' ) . the_category(', '); ?></p>
 </footer>
+</article>
