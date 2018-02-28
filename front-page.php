efddbc33536ac38f7561cbe0b53ab6b7f3003624
Andrej <an.sharapov90@gmail.com>
2018-02-28 15:17:38 +0300

    front-page.php

--- front-page.php
+++ front-page.php
@@ -1,7 +1,5 @@
 <?php get_header(); ?>
-<div class="container">
 <?php while ( have_posts() ) : the_post(); ?>
 <?php get_template_part('content') ?>
 <?php endwhile; ?>
-</div>
 <?php get_footer(); ?>
